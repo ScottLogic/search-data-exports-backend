8ce4d161b8d5b1fe41a3cdc7671fd38e39b46210
Roy Harrington <rharrington@scottlogic.com>
2019-07-09 16:08:24 +0100

    SDE-22: Allow lambda to create / write to CloudWatch

--- aws-infrastructure/modules/lambda/main.tf
+++ aws-infrastructure/modules/lambda/main.tf
@@ -6,9 +6,20 @@ data "archive_file" "search" {
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "${var.project}-lambda_exec_role"
+  name = "${var.project}-${var.environment}-lambda-exec-role"
 
   assume_role_policy = file("${path.module}/policies/lambda_exec_role.json")
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name = "${var.project}-${var.environment}-lambda-exec-policy"
+
+  policy = file("${path.module}/policies/lambda_exec_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role        = aws_iam_role.lambda_exec_role.name
+  policy_arn  = aws_iam_policy.lambda_logging.arn
 }
 
 resource "aws_lambda_function" "search_lambda" {
