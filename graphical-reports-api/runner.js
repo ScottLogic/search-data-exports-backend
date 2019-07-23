5aa1c5bc82b36fd2b2b7809fc9ea07edb6fcbcb2
Scott Woods <swoods@scottlogic.com>
2019-07-23 16:55:15 +0100

    sde-57 remove console spam from runner

--- graphical-reports-api/runner.js
+++ graphical-reports-api/runner.js
@@ -1,13 +1,12 @@
 /* This is not an actual test file, its a command line method to run the handler.  */
-const main = require('./index');
+const main = require("./index");
 
 const eventBody = {
-    body: '{ \"search\" : [ { \"dateRange\" : [\"2019-07-21\",\"2019-07-23\"] } ]}'
+  body: '{ "search" : [ { "dateRange" : ["2019-07-21","2019-07-23"] } ]}'
 };
 
-const CallBack = ( status, data) => {
-    console.log("CallBack Fired");
-    console.log(data);
-}
+const CallBack = (status, data) => {
+  console.log("CallBack Fired");
+};
 
-main.handler(eventBody,"context",CallBack);
+main.handler(eventBody, "context", CallBack);
