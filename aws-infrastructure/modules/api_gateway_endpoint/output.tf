8fffb9ea24a6a7059c4b43eac349c1c5b0ffb652
Scott Woods <swoods@scottlogic.com>
2019-07-24 11:25:22 +0100

    sde-77 terraform for new graphical reports

--- aws-infrastructure/modules/api_gateway_endpoint/output.tf
+++ aws-infrastructure/modules/api_gateway_endpoint/output.tf
@@ -1,3 +1,7 @@
 output "api_method_integration_id" {
   value = aws_api_gateway_integration.lambda_api_method_integration.id
 }
+
+output "api_resource_id" {
+  value = aws_api_gateway_resource.api_resource.id
+}
