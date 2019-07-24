e2465114cf1d7f05e9446942c81e0f7f21144b74
Roy Harrington <rharrington@scottlogic.com>
2019-07-24 14:13:47 +0100

    SDE64: Lambda to generate a report only, with no email element. Pretty much a copy of generator-report-api

---
+++ report-generator-api/app/csv-formatter.js
@@ -0,0 +1,28 @@
+class CSVFormatter {
+  formatHeader( searchResult ) {
+    let hits = searchResult.hits;
+
+    if (hits && hits.hits.length) {
+      return Object.keys(hits.hits[0]._source).join(',') + '\r\n';
+    }
+
+    return 'NoResults';
+  }
+
+  formatRows( searchResult ) {
+    const header = this.formatHeader(searchResult);
+    let hits = searchResult.hits;
+
+    const replacer = (key, value) => value === null ? '' : value 
+
+    if (hits && hits.hits.length) {
+      let header = Object.keys(hits.hits[0]._source);
+      return hits.hits.map(row => header.map(fieldName => JSON.stringify(row._source[fieldName], replacer)).join(',')).join('\r\n');
+    }
+
+    return '';
+  }
+
+}
+
+module.exports = CSVFormatter;
