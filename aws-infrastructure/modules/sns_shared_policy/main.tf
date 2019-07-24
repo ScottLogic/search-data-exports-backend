28a840c01baf4293c9e59c397358d77e4caf783a
Antreas Paschalis <AntreasPas@users.noreply.github.com>
2019-07-24 09:27:03 +0100

    SDE-50 Fix SNS Lambda Invoke Permissions & SNS Delivery Status Logging (#3)
    * SDE-50 removed sns invoke lambda permission qualifier, added execution policy
    
    * SDE-50 Better sns policies filenames, fixed LF
    
    * SDE-50 Fixed LF

--- aws-infrastructure/modules/sns_shared_policy/main.tf
+++ aws-infrastructure/modules/sns_shared_policy/main.tf
@@ -4,7 +4,7 @@ SNS Topic Delivery Status Logging
 ======*/
 resource "aws_iam_role" "aws_iam_role_success" {
   name = "${var.name_prefix}-SNSSuccessFeedback"
-  assume_role_policy = file("${path.module}/policies/feedback_policy.json")
+  assume_role_policy = file("${path.module}/policies/sns_exec_role.json")
   tags = {
     Name        = "${var.name_prefix}-SNSSuccessFeedback"
     Environment = var.environment
@@ -14,10 +14,30 @@ resource "aws_iam_role" "aws_iam_role_success" {
 
 resource "aws_iam_role" "aws_iam_role_failed" {
   name = "${var.name_prefix}-SNSFailureFeedback"
-  assume_role_policy = file("${path.module}/policies/feedback_policy.json")
+  assume_role_policy = file("${path.module}/policies/sns_exec_role.json")
   tags = {
     Name        = "${var.name_prefix}-SNSFailureFeedback"
     Environment = var.environment
     Project     = var.project
   }
+}
+
+data "template_file" "exec_policy" {
+  template = file("${path.module}/policies/sns_exec_policy.json")
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.name_prefix}-sns-exec-policy"
+
+  policy = data.template_file.exec_policy.template
+}
+
+resource "aws_iam_role_policy_attachment" "success_policy_attachment" {
+  role        = aws_iam_role.aws_iam_role_success.name
+  policy_arn  = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "failed_policy_attachment" {
+  role        = aws_iam_role.aws_iam_role_failed.name
+  policy_arn  = aws_iam_policy.lambda_policy.arn
 }
