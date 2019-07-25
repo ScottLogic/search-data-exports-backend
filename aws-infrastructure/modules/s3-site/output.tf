b19671ee16c5b1fa4b30aedb89d39adf9ef9eb83
Antreas Paschalis <apaschalis@scottlogic.com>
2019-07-25 16:26:40 +0100

    SDE-87 Removed unused s3-site module output

--- aws-infrastructure/modules/s3-site/output.tf
+++ aws-infrastructure/modules/s3-site/output.tf
@@ -1,11 +1,3 @@
-output "arn" {
-  value = aws_s3_bucket.sde-bucket-site.arn
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.sde-bucket-site.bucket
-}
-
 output "site_endpoint" {
   value = aws_s3_bucket.sde-bucket-site.website_endpoint
 }
