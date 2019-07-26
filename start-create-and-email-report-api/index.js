264f03cd844fd685ef0d693360c7b5c947ebfede
OliverForeman <oliverforeman@btinternet.com>
2019-07-26 11:12:46 +0100

    SDE-88 Created lambda to start step function

---
+++ start-create-and-email-report-api/index.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const stepFunctions = new AWS.StepFunctions();
+
+exports.handler = async event => {
+  const message = event.Records[0].Sns.Message;
+
+  const data = JSON.parse(message);
+
+  const input = {
+    emailAddress: data.parameters.emailAddress,
+    searchCriteria: data.searchCriteria
+  };
+
+  const params = {
+    stateMachineArn: process.env.CREATE_AND_EMAIL_REPORT_ARN,
+    input: JSON.stringify(input)
+  };
+
+  stepFunctions.startExecution(params, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    }
+    else {
+      console.log(data);
+    }
+  });
+};
