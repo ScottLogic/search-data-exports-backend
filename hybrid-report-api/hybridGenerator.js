8ece40722cc6825aac25797d9a73f369ba033c6e
Scott Woods <swoods@scottlogic.com>
2019-07-26 15:17:24 +0100

    sde-91 svg generation added

--- hybrid-report-api/hybridGenerator.js
+++ hybrid-report-api/hybridGenerator.js
@@ -1,20 +1,25 @@
 const ESSearch = require(`./common/search`);
+const SVGBuilder = require(`./common/svgbuilder`);
 
 class HybridGenerator {
   constructor(ConnectionOptions) {
-    this._search = new ESSearch(ConnectionOptions);
+    this._search = new ESSearch(ConnectionOptions);    
   }
 
   async generateReport(paramJSON = {}) {
-    const reportData = await this.getReportData(paramJSON);    
-    return reportData;
+    const reportData = await this.getReportData(paramJSON);  
+    const svgData = await this.buildSVG( reportData );
+    return svgData;
   }
 
   async getReportData(paramJSON) {
     return await this._search.search(this.buildRequestJSON(paramJSON));
   }
 
-  async buildSVG() {}
+  async buildSVG( ESResults ) {
+      const svgbuilder = new SVGBuilder();
+      return await svgbuilder.build( ESResults );
+  }
 
   async buildHTML() {}
 
@@ -36,7 +41,7 @@ class HybridGenerator {
           types_count: {
             terms: {
               field: "Tags.keyword",
-              size: 30
+              size: 20
             }
           }
         },
