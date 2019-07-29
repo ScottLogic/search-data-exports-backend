f929c0b7f9d809623724179e4dcb2435860d00cb
Antreas Paschalis <apaschalis@scottlogic.com>
2019-07-29 17:21:54 +0100

    SDE-99 start-create-and-email-report-api lint & circleci

--- start-create-and-email-report-api/index.js
+++ start-create-and-email-report-api/index.js
@@ -1,12 +1,13 @@
 const AWS = require('aws-sdk');
+
 const stepFunctions = new AWS.StepFunctions();
 
 const callbackHeaders = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*"
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*'
 };
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event, _, callback) => {
   const message = event.Records[0].Sns.Message;
 
   const data = JSON.parse(message);
@@ -21,24 +22,26 @@ exports.handler = async (event, context, callback) => {
     input: JSON.stringify(input)
   };
 
-  await stepFunctions.startExecution(params).promise().then(data => {
-    console.log('Step function execution response:', data);
+  await stepFunctions.startExecution(params).promise()
+    .then((executionData) => {
+      console.log('Step function execution response:', data);
 
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(data),
-      headers: callbackHeaders
-    });
-  }).catch(err => {
-    console.log(err, err.stack);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(executionData),
+        headers: callbackHeaders
+      });
+    })
+    .catch((err) => {
+      console.log(err, err.stack);
 
-    callback(null, {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Error occured in step function execution',
-        errorMessage: err
-      }),
-      headers: callbackHeaders
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Error occured in step function execution',
+          errorMessage: err
+        }),
+        headers: callbackHeaders
+      });
     });
-  });
 };
