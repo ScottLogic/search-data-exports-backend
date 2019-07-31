dbc92559a69185d5f84c1037217d11b6b6d435b9
Roy Harrington <rharrington@scottlogic.com>
2019-07-31 08:34:29 +0100

    SDE-108: removed used SNS modules

--- aws-infrastructure/modules/sns_shared_policy/output.tf
+++
@@ -1,7 +0,0 @@
-output "sns_success_feedback_iam_role_arn" {
-  value = aws_iam_role.aws_iam_role_success.arn
-}
-
-output "sns_failure_feedback_iam_role_arn" {
-  value = aws_iam_role.aws_iam_role_failed.arn
-}
