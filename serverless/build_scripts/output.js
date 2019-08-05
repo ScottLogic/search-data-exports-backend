2d4c7e8089c9c0f6cab53d93f88a6bb2339ff1dd
Antreas Paschalis <apaschalis@scottlogic.com>
2019-08-05 15:28:46 +0100

    SDE-116: Test injecting gateway url to circleci - being stupid 4 fix

--- serverless/build_scripts/output.js
+++ serverless/build_scripts/output.js
@@ -4,10 +4,10 @@ function handler(data) {
   fs.mkdirSync('./env');
 
   const restApiUrl = { REACT_APP_API_URL: data.ServiceEndpoint };
-  fs.writeFileSync('./envs/update_rest_api_url.json', JSON.stringify(restApiUrl));
+  fs.writeFileSync('./env/update_rest_api_url.json', JSON.stringify(restApiUrl));
 
   const webSocketUrl = { REACT_APP_WEBSOCKET_ENDPOINT: data.ServiceEndpointWebsocket };
-  fs.writeFileSync('./envs/update_websocket_url.json', JSON.stringify(webSocketUrl));
+  fs.writeFileSync('./env/update_websocket_url.json', JSON.stringify(webSocketUrl));
 }
 
 module.exports = { handler };
