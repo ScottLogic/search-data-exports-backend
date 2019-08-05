6b1e1a8c6388b07e8ffe955787be3584daf12ef2
Antreas Paschalis <apaschalis@scottlogic.com>
2019-08-05 16:33:28 +0100

    SDE-116: Post data fix, trigger frontend build API call

--- serverless/build_scripts/output.js
+++ serverless/build_scripts/output.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 
+function getNameValuePair(envVariableName, envVariableValue) {
+  return {
+    name: envVariableName,
+    value: envVariableValue
+  };
+}
+
 function handler(data) {
   fs.mkdirSync('./env');
 
-  const restApiUrl = { REACT_APP_API_URL: data.ServiceEndpoint };
-  fs.writeFileSync('./env/update_rest_api_url.json', JSON.stringify(restApiUrl));
+  const restApiUrlEnv = getNameValuePair('REACT_APP_API_URL', data.ServiceEndpoint);
+  fs.writeFileSync('./env/update_rest_api_url.json', JSON.stringify(restApiUrlEnv));
 
-  const webSocketUrl = { REACT_APP_WEBSOCKET_ENDPOINT: data.ServiceEndpointWebsocket };
-  fs.writeFileSync('./env/update_websocket_url.json', JSON.stringify(webSocketUrl));
+  const webSocketUrlEnv = getNameValuePair('REACT_APP_WEBSOCKET_ENDPOINT', data.ServiceEndpointWebsocket);
+  fs.writeFileSync('./env/update_websocket_url.json', JSON.stringify(webSocketUrlEnv));
 }
 
 module.exports = { handler };
