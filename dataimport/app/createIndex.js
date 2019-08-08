ed1b08a35d04db0d0653bf8629e53dd949cd310e
OliverForeman <oliverforeman@btinternet.com>
2019-08-08 16:34:20 +0100

    SDE-140 Added function to create index for digest searches

---
+++ dataimport/app/createIndex.js
@@ -0,0 +1,35 @@
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'https://search-sde-dev-es-test-domain-hplsrf7hatpadkuvakzrf3j7wq.eu-west-1.es.amazonaws.com' });
+
+const ES_INDEX = 'digests';
+const ES_TYPE = 'digest';
+
+const createIndex = async () => {
+  const indexExists = await client.indices.exists({ index: 'digests' });
+
+  if (!indexExists.body) {
+    await client.indices.create({
+      index: ES_INDEX,
+      body: createBody()
+    });
+  }
+};
+
+const createBody = () => ({
+  mappings: {
+    [ES_TYPE]: {
+      properties: {
+        search: {
+          properties: {
+            query: { type: 'percolator' },
+            userID: { type: 'text' }
+          }
+        },
+        Content: { type: 'text' },
+        Tags: { type: 'text' }
+      }
+    }
+  }
+});
+
+createIndex().catch(console.log);
