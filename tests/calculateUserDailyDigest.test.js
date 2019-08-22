aa749095b69554c9e937eaa34d03c8beab3e4292
Antreas Paschalis <apaschalis@scottlogic.com>
2019-08-22 10:56:37 +0100

    SDE-170: Missing LF added

---
+++ tests/calculateUserDailyDigest.test.js
@@ -0,0 +1,7 @@
+import { handler } from '../functions/calculateUserDailyDigest';
+
+describe('calculateUserDailyDigest function', () => {
+  it('passes a test', async () => {
+    expect(true).toEqual(true);
+  });
+});
